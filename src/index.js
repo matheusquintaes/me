6f002cb596386ca16521370a1e23e8d13e3eaf82
matheusquintaes <matheus.quintaes@gmail.com>
2017-08-01 11:14:43 -0300

    Adicionando animejs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './style.css';
 import './index.html'
+import anime from 'animejs'
+
+var lineDrawing = anime({
+  targets: '#lineDrawing path, #lineDrawing polygon , #lineDrawing line ',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  delay: function(el, i) { return i * 100 },
+  direction: 'alternate',
+  loop: true 
+});
